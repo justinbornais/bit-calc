69b5872f49527fd7793e4a833f072a5ff407b745
Justin135 <justin.bornais@gmail.com>
2023-04-05 00:22:47 -0400

    Initial commit.

---
+++ script.js
@@ -0,0 +1,27 @@
+// Global variables.
+/*
+var scrwidth = window.innerWidth;
+var scrheight = window.innerHeight;
+
+
+const stage = acgraph.create('bits1'); // Create the stage for the first row of bits.
+const stage2 = acgraph.create('bits2'); // Create the stage for the second row of bits.
+var element = document.getElementById('bits1');
+var element2 = document.getElementById('bits2');
+var rect = element.getBoundingClientRect();
+var rect2 = element2.getBoundingClientRect();
+
+var width = rect.right - rect.left;
+var height = rect.bottom - rect.top;
+var width2 = rect2.right - rect2.left;
+var height2 = rect2.bottom - rect2.top;
+var outerThick = 0.03 * height;
+
+console.log(height)
+console.log(height2)
+console.log(window.innerWidth)
+console.log(window.innerHeight)
+
+stage.rect(0, 0, width, height).fill("#999999").stroke("black", outerThick);
+stage2.rect(0, 0, width, height).fill("#888888").stroke("black", outerThick);
+*/
